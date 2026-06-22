484a91e683976bfd86fe41a4726483c8691b74ee
kbyrne03 <kb106@student.london.ac.uk>
2026-06-22 16:49:05 +0100

    Added initial code to read novels by compiling a list using the os import module to grab novel titles. Also added initial code to the nltk function to tokenise text and words getting bot the types and ttr of the text

--- PartOne.py
+++ PartOne.py
@@ -2,15 +2,16 @@
 
 # Note: The template functions here and the dataframe format for structuring your solution is a suggested but not mandatory approach. You can use a different approach if you like, as long as you clearly answer the questions and communicate your answers clearly.
 
+import string
 import nltk
 import spacy
 from pathlib import Path
+import pandas as pd
 
+import os
 
 nlp = spacy.load("en_core_web_sm")
 nlp.max_length = 2000000
-
-
 
 def fk_level(text, d):
     """Returns the Flesch-Kincaid Grade Level of a text (higher grade is more difficult).
@@ -43,8 +44,19 @@ def count_syl(word, d):
 def read_novels(path=Path.cwd() / "texts" / "novels"):
     """Reads texts from a directory of .txt files and returns a DataFrame with the text, title,
     author, and year"""
-    pass
+    novels = os.listdir(f"{Path.cwd()}/texts/novels")
+    # vectorise list of novels
+    novels = [n for n in novels]
 
+    for val in novels:
+        print(f"Novel title {val}")
+        print(f"Title of novel is ", val)
+        columns = ["text", "title", "author", "year"]
+        df = pd.DataFrame(columns=columns)
+        df.sort_values(by="year", inplace=True)
+        print(df)
+        pass
+    return
 
 def parse(df, store_path=Path.cwd() / "pickles", out_name="parsed.pickle"):
     """Parses the text of a DataFrame using spaCy, stores the parsed docs as a column and writes 
@@ -53,7 +65,33 @@ def parse(df, store_path=Path.cwd() / "pickles", out_name="parsed.pickle"):
 
 
 def nltk_ttr(text):
-    """Calculates the type-token ratio of a text. Text is tokenized using nltk.word_tokenize."""
+    """Calculates the type-token ratio of a text. Text is tokenized using nltk.word_tokenize. Referred to: https://www.kaggle.com/code/kelixirr/tokenization-text-processing-in-nlp"""
+    
+    # Split all punctuation in text into its own tokens
+    word_punctuation_tokenizer = nltk.WordPunctTokenizer()
+    word_punctuation_tokens = word_punctuation_tokenizer.tokenize(text)
+    # Clean punctuation - referred to: https://www.geeksforgeeks.org/python/string-punctuation-in-python/
+    cleantxt = [t for t in word_punctuation_tokens if t not in string.punctuation]
+
+    # Tokenise cleaned punctuation from txt
+    tokenised_documents = nltk.word_tokenize(cleantxt)
+
+    # Set types from tokenised documents
+    types = set(tokenised_documents)
+    #Use ttr to divide the number of words by the number of unique words. If there are no tokens, return 0.
+    ttr = len(types).lower() / len(tokenised_documents) if tokenised_documents else 0
+    #Define dictionary and now map values to the ttr values
+    dictionary = text.dictionary(tokenised_documents)
+
+    dict.mapping = {
+        "title": ttr
+    }
+
+    return {
+        "tokens": len(tokens),
+        "types": len(types),
+        "ttr": ttr
+    }
     pass
 
 
@@ -76,15 +114,13 @@ def get_fks(df):
 
 #.. add functions for part (e) here
 
-
-
 if __name__ == "__main__":
     """
     uncomment the following lines to run the functions once you have completed them
     """
-    # path = Path.cwd() / "texts" / "novels"
-    # print(path)
-    # df = read_novels(path) # this line will fail until you have completed the read_novels function above.
+    path = Path.cwd() / "texts" / "novels"
+    print(path)
+    df = read_novels(path) # this line will fail until you have completed the read_novels function above.
     # print(df.head())
     # nltk.download("cmudict")
     # parse(df)
