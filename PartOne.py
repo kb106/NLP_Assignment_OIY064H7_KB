73c4f85da87d9f1a463b8f8d05d680f10cf42211
kbyrne03 <kb106@student.london.ac.uk>
2026-06-24 14:49:16 +0100

    Optimised nltk TTR ratio function to use the nltk tokenizer call instead of a direct call to process text using nlp, updated synt ax in code to reflect use of ntlk. Completed FK function to tokenize sentences, words and number of syllables in words on average using the FK formula, the output of which is returned by napping nov el titles to their corresponding FK scores (ease of text readability) via the get_fks function

--- PartOne.py
+++ PartOne.py
@@ -1,14 +1,13 @@
 #NLP assessment template 2026
 
 # Note: The template functions here and the dataframe format for structuring your solution is a suggested but not mandatory approach. You can use a different approach if you like, as long as you clearly answer the questions and communicate your answers clearly.
-
-import datetime
-import string
 import nltk
 import spacy
 from pathlib import Path
 import pandas as pd
+
 import os
+from curses.ascii import isdigit
 
 try:
     nlp = spacy.load("en_core_web_sm")
@@ -24,6 +23,11 @@ def fk_level(text, d):
     """Returns the Flesch-Kincaid Grade Level of a text (higher grade is more difficult).
     Requires a dictionary of syllables per word.
 
+    Implements Flesch Formula:
+        Reading Ease score = 206.835 - (1.015 × ASL) - (84.6 × ASW)
+
+        ASL = Avg Sentence Length
+        ASW = Avg Word Length
     Args:
         text (str): The text to analyze.
         d (dict): A dictionary of syllables per word.
@@ -31,6 +35,50 @@ def fk_level(text, d):
     Returns:
         float: The Flesch-Kincaid Grade Level of the text. (higher grade is more difficult)
     """
+    #  Referred to WK4 - Sentences and https://datawarrior.wordpress.com/2016/03/29/flesch-kincaid-readability-measure/
+    # Set fkresults as the dictionary to add to
+    fkresults = {}
+    num_syllables = 0
+    total_syllables = 0
+
+    # find the length of words via the tokenized text and filter out punctuation
+    tokens = [token.lower() for token in nltk.word_tokenize(text) if token.isalpha()]
+    num_words = len(tokens)
+    # find the the length of sentences based on the number of sentences
+    num_sentences = len(nltk.sent_tokenize(text))
+    # initialise and get the syllables for each word via Carnegie-Mellon University (CMU) dictionary
+    sylldict = nltk.corpus.cmudict.dict()
+
+    for word in tokens:
+      try:
+        if word in sylldict:
+            # Referenced syllable count pattern from: https://gist.github.com/drinks/2483508
+            phonemes =  sylldict[word][0]
+            num_syllables += len([i for i in phonemes if i[-1].isdigit()])
+        else:
+            total_syllables += 1
+          # Handle words missing from dictionary...    
+      except KeyError:
+            num_syllables = 1.66 
+
+    # Handle cases where the number of words or sentences returns NULL
+    if num_words == 0 or num_sentences == 0:
+        return 0.0
+
+    # Formula for Averages and FK
+    avg_words_sentence = num_words / num_sentences if num_sentences > 0 else 0 # avergae out the number of words in each sentence
+    avg_syll_word = num_syllables / num_words if num_words > 0 else 0 # average out the number of syllables per word
+ 
+    # Formula for reading Ease score = 206.835 - (1.015 × ASL) - (84.6 × ASW)
+    fkresults = 206.835 - (1.015 * avg_words_sentence) - (84.6 * avg_syll_word)
+    # for i, row in text.iterrows():
+    #     fkresults[row["title"]] = round(fk_level(row["text"], cmudict), 4)
+
+    # Create dictionary of syllables per word
+    # syllable_count={
+
+    # }
+    return fkresults 
     pass
 
 def count_syl(word, d):
@@ -102,10 +150,10 @@ def nltk_ttr(text):
     """Calculates the type-token ratio of a text. Text is tokenized using nltk.word_tokenize. Referred to: https://www.kaggle.com/code/kelixirr/tokenization-text-processing-in-nlp"""
     # Use spacy, reference: https://spacy.io/models/en
     # Reference global variable for spacy to extract and tokenise text
-    output_text = nlp(text)
+    output_text = nltk.word_tokenize(text)
     # For TTR ration we want to measure the value returned between 1 (highly diverse and unique set of words) and 0 or higher repetition
     # Vectorize token call, ensure text is not case-sensitive and use is_alpha to filter out punctuation before parsing texts to get TTR
-    tokens = [token.text.lower() for token in output_text if token.is_alpha]
+    tokens = [token.lower() for token in output_text if token.isalpha]
 
     # Handle cases where there are no tokens and return null 
     if not tokens:
@@ -136,6 +184,7 @@ def get_fks(df):
     cmudict = nltk.corpus.cmudict.dict()
     for i, row in df.iterrows():
         results[row["title"]] = round(fk_level(row["text"], cmudict), 4)
+    print(f"Novel title and FKS score: {results}")
     return results
 
 #.. add functions for part (e) here
@@ -148,8 +197,9 @@ if __name__ == "__main__":
     print(path)
     df = read_novels(path) # this line will fail until you have completed the read_novels function above.
     get_ttrs(df)
+    nltk.download("cmudict")
+    get_fks(df)
     # print(df.head())
-    # nltk.download("cmudict")
     # parse(df)
     # print(df.head())
     # print(get_ttrs(df))
