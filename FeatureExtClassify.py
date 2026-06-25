4952732fc0070ea8d4ba5450e1d6a19c749a69da
kbyrne03 <kb106@student.london.ac.uk>
2026-06-25 20:46:07 +0100

    Setup initial file and function with import to process inaugural presidential speeches by setting the DIR and extensions to process and then setup calls to read the files and output the speech in raw format

---
+++ FeatureExtClassify.py
@@ -0,0 +1,30 @@
+'''Python script to process and extract inaugural speeches, extract and classify features'''
+
+import os
+import glob
+
+# Referred to Lab 3 Part 1 processing inaugural speeches - NLP course
+
+# Sort files once path to files to process are referenced
+files = sorted(glob.glob(os.path.join('texts/', '*.csv' )))
+
+def process_inaugural_speeches(files):
+ # Define a corpus to process
+ inaugural_speeches = {}
+ print(f"Hello!")
+
+ for speech_file in files:
+   filename = os.path.basename(speech_file)
+
+   with open(speech_file, 'r', encoding="utf-8", errors="ignore") as file:
+     speech_content = file.read()
+     inaugural_speeches[filename] = speech_content
+
+     print(speech_content)
+     
+ 
+if __name__ == "__main__":
+    """
+    uncomment the following lines to run the functions once you have completed them
+    """
+process_inaugural_speeches(files) 
