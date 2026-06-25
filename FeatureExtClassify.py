e1450fcccb9263bbcbb4680422307cfd53256752
kbyrne03 <kb106@student.london.ac.uk>
2026-06-25 22:22:38 +0100

    Restructured the function to process speeches by moving the file call into __main__, then called each column required and set as a dataframe, and passed the df to the function to iterate through the speech csv file rows/cols

--- FeatureExtClassify.py
+++ FeatureExtClassify.py
@@ -6,31 +6,30 @@ import pandas as pd
 
 # Referred to Lab 3 Part 1 processing inaugural speeches - NLP course
 
-# Sort files once path to files to process are referenced
-files = sorted(glob.glob(os.path.join('texts/', '*.csv' )))
-
-def process_inaugural_speeches(files):
+def process_inaugural_speeches(df):
  # Define a corpus to process
  inaugural_speeches = {}
 
- for speech_file in files:
-   filename = os.path.basename(speech_file)
-
-   with open(speech_file, 'r', encoding="utf-8", errors="ignore") as file:
-     speech_content = pd.read_csv(file, usecols=["speech", "party", "major_heading", "year"])
-     inaugural_speeches[filename] = speech_content
-
-     for i, row in speech_content.iterrows():
+ for i, row in df.iterrows():
+      
       # Define columns to process 
       rtn_speech = row["speech"]
       rtn_byline =  row["major_heading"]
       rtn_year =  row["year"]
       rtn_party =  row["party"]
 
-     print(f"Political party: {rtn_party} \n-----\n  Year: {rtn_year} \n-----\n  Speech Byline: {rtn_byline}  \n-----\n  Speech: {rtn_speech}")
+      print(f"Political party: {rtn_party} \n-----\n  Year: {rtn_year} \n-----\n  Speech Byline: {rtn_byline}  \n-----\n  Speech: {rtn_speech}")
  
 if __name__ == "__main__":
     """
     uncomment the following lines to run the functions once you have completed them
     """
-process_inaugural_speeches(files) 
+# Store the file in a DataFeame, then sort files once path to file to processis referenced
+csv_file = sorted(glob.glob(os.path.join('texts/', 'hansard10000.csv' )))
+csv_to_process = csv_file[0]
+try:
+  with open(csv_to_process, 'r', encoding="utf-8", errors="ignore") as file:
+   df = pd.read_csv(file, usecols=["speech", "party", "major_heading", "year"])
+   process_inaugural_speeches(df)
+except ValueError as e:
+  print(f"Unable to process {csv_to_process} {e}")
